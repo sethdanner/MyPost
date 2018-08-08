144d89f4f3eaed932be574cfac2ad0a5177021b3
Seth <sethdanner@gmail.com>
2018-08-07 18:33:50 -0600

    Added Post model.

---
+++ MyPost/Post.swift
@@ -0,0 +1,23 @@
+//
+//  Post.swift
+//  MyPost
+//
+//  Created by Seth Danner on 8/7/18.
+//  Copyright © 2018 Seth Danner. All rights reserved.
+//
+
+import Foundation
+
+struct Post: Codable {
+    
+    let username: String
+    let text: String
+    let timestamp: TimeInterval
+    
+    init(username: String, text: String, timestamp: TimeInterval = Date().timeIntervalSince1970) {
+        
+        self.username = username
+        self.text = text
+        self.timestamp = timestamp
+    }
+}
