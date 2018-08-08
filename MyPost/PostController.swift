2c5a38984d90e14841ae772069e915090ee50213
Seth <sethdanner@gmail.com>
2018-08-07 18:36:48 -0600

    Created PostController class.

---
+++ MyPost/PostController.swift
@@ -0,0 +1,15 @@
+//
+//  PostController.swift
+//  MyPost
+//
+//  Created by Seth Danner on 8/7/18.
+//  Copyright © 2018 Seth Danner. All rights reserved.
+//
+
+import Foundation
+
+class PostController {
+    
+    var posts: [Post] = []
+    
+}
